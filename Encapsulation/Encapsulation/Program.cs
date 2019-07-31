35bf2801877580ccf738dc46320950d5c16e18c8
9977559 <dann.hollander@gmail.com>
2019-07-31 15:56:43 +1200

    Update 2

---
+++ Encapsulation/Encapsulation/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee[] people = new Employee[]
+            {
+                new Employee("bob", "0220220222", "k@gmail.com", "123-456-789", "01-1234-12345678-99", "bobby", "B.0.B"),
+                new Employee("Steve", "0220220522", "S@gmail.com", "123-896-789", "01-9934-12345678-99", "Steveis", "StevesPassword1"),
+                new Employee("Kyle", "02202266666", "kyle@gmail.com", "123-456-888", "01-1234-12348523-99", "Monsterk", "DryWallDieWall"),
+                new Employee("Joseph", "0228888222", "j@gmail.com", "895-456-789", "01-1234-1258978-99", "Joseph", "Password1"),
+
+            };
+            foreach(Employee x in people)
+            {
+                Console.WriteLine($"\n {x.Workers()}");
+            }
+            Console.ReadLine();
+        }
+    }
+
+    class Employee
+    {
+        private string fullname { get; set; }
+        private string phone { get; set; }
+        private string email { get; set; }
+        private string ird { get; set; }
+        private string bank { get; set; }
+        private string uname { get; set; }
+        private string pass { get; set; }
+
+        public Employee()
+        {
+        }
+        public Employee(string _fullname, string _phone, string _email, 
+            string _ird, string _bank, string _uname, string _pass)
+        {
+            fullname = _fullname;
+            phone = _phone;
+            email = _email;
+            ird = _ird;
+            bank = _bank;
+            uname = _uname;
+            pass = _pass;
+        }
+
+        public string Workers()
+        {
+
+            return $"FullName: {fullname}\n" +
+                $"Phone: {phone}\n" +
+                $"Email: {email}\n" +
+                $"Ird: {ird}\n" +
+                $"Bank: {bank}\n" +
+                $"User Name: {uname}\n" +
+                $"Password: {pass}\n";
+        }
+    }
+
+}
--- Excersise 2/Excersise 2/Program.cs
+++ Excersise 2/Excersise 2/Program.cs
@@ -11,25 +11,40 @@ namespace Excersise_2
         static void Main(string[] args)
         {
             /*EXCERSISE 2*/
-           
-            
+
+
             /*Q2*/
+            // Create a program that will create an array of 100 random numbers from 0 to 100. 
+            //The program then needs to display the sum of all of the numbers in the array.
 
-            
-            
-            
-            
-            
-            
-            
-            /*Q4*/
-            Console.WriteLine($"Please enter a number: {i+1}");
-            int num1 = Convert.ToInt32(Console.ReadLine());
-                
-
-
+            int[] nums = new int[100];
+            nums = Populate(nums);
+            Console.WriteLine("Array contents:");
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.Write($"{nums[i]}");
+                if ((i + 1) % 5 == 0) ;
+                Console.WriteLine();
+            }
+            Console.WriteLine($"\n The sum of the numbers in the array is : {Sum(nums)}");
+            Console.ReadLine();
         }
-
+        static int[] Populate(int[] nums)
+        {
+            Random rand = new Random();
+            for(int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = rand.Next(100);
+            }
+            return nums;
+        }
+        static int Sum(int[] nums)
+        {
+            int total = 0;
+            foreach (int x in nums)
+                total = +x;
+            return total;
+        }
 
 
     }
